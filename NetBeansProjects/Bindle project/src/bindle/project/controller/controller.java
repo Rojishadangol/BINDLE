3a24b8a4078ccdf6c83584cfb4f1b7cb4f28fcbc
ushadhakal <ushadhakal@localhost>
2025-06-09 08:40:43 +0545

    controller for book

---
+++ NetBeansProjects/Bindle project/src/bindle/project/controller/controller.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bindle.project.controller;
+import bindle.project.dao.userdao;
+import model.Book;
+
+import java.util.List;
+/**
+ *
+ * @author ushadhakal
+ */
+public class controller {
+    
+
+
+public class BookController {
+    private userdao userdao;
+
+    public BookController() {
+        userdao = new userdao();
+    }
+
+    public List<Book> search(String keyword) {
+        return userdao.searchBooks(keyword);
+    }
+}
+
+}
+
