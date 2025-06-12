e4163ffce8f742089a9192dd6d789fc2add85a56
usha44 <dhakalusha524@gmail.com>
2025-06-12 09:58:54 +0545

    navigation controller new class added

---
+++ src/bindle_project/Controller/NavigationController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bindle_project.Controller;
+import bindle_project.View.CartScreen;
+/**
+ *
+ * @author ushadhakal
+ */
+public class NavigationController {
+    public void goToCartScreen() {
+        CartScreen cartScreen = new CartScreen();
+        cartScreen.setVisible(true);
+    }
+    
+}
