825d784aff5b8b190486c89581e736905e7ed53f
diya-neupane <439diyaneupane@gmail.com>
2025-06-18 09:16:04 +0545

    code of logininnterface

--- src/bindle_project/Controller/LoginInterfaceController.java
+++ src/bindle_project/Controller/LoginInterfaceController.java
@@ -2,12 +2,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+// LoginController.java
 package bindle_project.Controller;
 
-/**
- *
- * @author diyan
- */
+import bindle_project.View.LoginInterfacee;
+import bindle_project.View.EditProfile;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
 public class LoginInterfaceController {
-    
+    private LoginInterfacee LoginInterface;
+
+    public LoginInterfaceController(LoginInterfacee LoginInterfacee) {
+        this.LoginInterface = LoginInterfacee;
+
+        this.LoginInterface.getSaveButton().addActionListener(new LoginButtonListener());
+        this.LoginInterface.getCancelButton().addActionListener(new CancelButtonListener());
+    }
+
+    class LoginButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String username = LoginInterface.getNameField().getText();
+
+            if (username.equals("admin") ) {
+                JOptionPane.showMessageDialog(null, "Login successful!");
+                LoginInterface.dispose(); // Close login form
+                new EditProfile().setVisible(true); // Open edit profile view
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid username or password");
+            }
+        }
+    }
+
+    class CancelButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            LoginInterface.dispose();
+        }
+    }
 }
+
+
