75cf332d64b1e7a9a1d4d1ee5ddcecacb8aaf7f2
Shady721 <bhattaraibijaya20@gmail.com>
2025-06-22 09:15:41 +0545

    changes

---
+++ src/dao/SecurePurchaseDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.sql.*;
+import model.SecurePurchase;
+
+public class SecurePurchaseDAO {
+    private Connection conn;
+
+    public SecurePurchaseDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public boolean insertPurchase(SecurePurchase purchase) throws SQLException {
+        String sql = "INSERT INTO secure_purchases (buyer_id, seller_id, book_id, status) VALUES (?, ?, ?, ?)";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, purchase.getBuyerId());
+        stmt.setInt(2, purchase.getSellerId());
+        stmt.setInt(3, purchase.getBookId());
+        stmt.setString(4, purchase.getStatus());
+        return stmt.executeUpdate() > 0;
+    }
+}
